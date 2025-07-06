69f92039465ce25ca8c659a6c8e769041981b180
Admin <Admin@DESKTOP-C83HAAK>
2025-07-06 14:10:28 -0700

    day2

---
+++ DAY_TWO/src/com/tnsif/daytwo/loopsDemo/ForLoopDemo.java
@@ -0,0 +1,13 @@
+package com.tnsif.daytwo.loopsDemo;
+
+public class ForLoopDemo {
+
+	public static void main(String[] args) {
+
+		for(int i=1; i<=100 ; i++)
+	
+		{
+			System.out.print("value of i :"+ i + " \n ");
+		}
+	}
+}
---
+++ DAY_TWO/src/com/tnsif/daytwo/loopsDemo/WhileDemo.java
@@ -0,0 +1,19 @@
+package com.tnsif.daytwo.loopsDemo;
+
+public class WhileDemo {
+
+public static void main(String[] args) {
+
+		
+		int i=10
+				;// user value/ user input 
+		
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+		
+	}
+
+}
