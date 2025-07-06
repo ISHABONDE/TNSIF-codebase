ee6577d7dd7f28340f50ac19c74010ac1f1e9940
Admin <Admin@DESKTOP-C83HAAK>
2025-07-06 14:46:41 -0700

    Day6

---
+++ DAY_SIX/src/com/tnsif/daysix/staticmethod/EmployeeDemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.daysix.staticmethod;
+
+public class EmployeeDemo {
+
+        public static void main(String[] args) {
+		
+		
+		//Creating first object of the class 
+		
+		Employee e=new Employee("Isha", 1001);
+		System.out.println(e);
+
+	}
+}
