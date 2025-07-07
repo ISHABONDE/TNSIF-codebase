dd1e575a709962638405882f0fbd395f8c00bf91
Admin <Admin@DESKTOP-C83HAAK>
2025-07-07 10:24:19 +0530

    Day1

--- demo1/src/tnsif/FirstProgram.java
+++ demo1/src/tnsif/FirstProgram.java
@@ -3,5 +3,6 @@ package tnsif;
 public class FirstProgram {
            public static void main(String[] args) {
 			System.out.println("............hello friends!!........");
+			System.out.println("isha here...!");
 		}
 }
