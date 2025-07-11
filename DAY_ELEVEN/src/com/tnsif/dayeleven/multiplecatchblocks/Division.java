b8915e3145ea0aedf8c8bccd22863305799f1ac5
Admin <Admin@DESKTOP-C83HAAK>
2025-07-11 10:26:31 +0530

    Day11

--- DAY_ELEVEN/src/com/tnsif/dayeleven/multiplecatchblocks/Division.java
+++ DAY_ELEVEN/src/com/tnsif/dayeleven/multiplecatchblocks/Division.java
@@ -3,25 +3,41 @@ package com.tnsif.dayeleven.multiplecatchblocks;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+//multiple catch block 
 public class Division {
- public static void Divide() {
-	 int a,b,c;
-	 Scanner sc= new Scanner(System.in);
-	 System.out.println("enter two numbers:");
-	 
-	 try {
-		 a=sc.nextInt();
-		 b=sc.nextInt();
-		 c=a/b;
-		 System.out.println("division is:"+c);
-		 
-	 }
-	 catch(InputMismatchException e) {
-		 System.out.println(e.getMessage());
-	 }
-	 
-	 catch(ArithmeticException | ArrayIndexOutOfBoundsException e) {
-		 System.out.println(e.getMessage());
-	 }
- }
+
+	public static void divide()
+	{
+		int a,b,c;
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter two number: ");
+		
+		try {
+			a=sc.nextInt();
+			b=sc.nextInt();
+			
+			c=a/b;
+			
+			System.out.println("Division is : "+c);
+		}
+		catch (InputMismatchException e)
+		{
+			System.err.println(e.getMessage());
+		}
+		catch(ArithmeticException | ArrayIndexOutOfBoundsException e)
+		{
+			System.err.println(e.getMessage());
+		}
+		catch (Exception e)
+		{
+			System.err.println(e.getMessage());
+		}
+		
+		finally {
+			sc.close();
+			System.out.println("Finally block ");
+		}
+		
+	}
 }
